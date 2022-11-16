497f38f7683f878d6b94f5e4e52614ed35dbcf61
MedzikUser <medzik@duck.com>
2022-11-16 20:57:09 +0100

    feat(database): add `DeletedAt` column

--- database/model.go
+++ database/model.go
@@ -2,9 +2,12 @@ package database
 
 import (
 	"time"
+
+	"gorm.io/plugin/soft_delete"
 )
 
 type Model struct {
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	DeletedAt soft_delete.DeletedAt
 }
--- go.mod
+++ go.mod
@@ -46,4 +46,5 @@ require (
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/plugin/soft_delete v1.2.0 // indirect
 )
